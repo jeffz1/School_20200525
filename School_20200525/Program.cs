be3bde7525aab9ee6670b59c7a6ed6e5ded5ce52
jeffz1 <jm@jeffz.com>
2020-05-25 21:04:11 -0400

    Add project files.

---
+++ School_20200525/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace School_20200525
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
